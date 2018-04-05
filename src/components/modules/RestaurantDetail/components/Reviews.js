fa7c5473d07c5aaa005271a8ad1356796e6ce1b2
Sander Vispoel <sandervispoel@hotmail.com>
2018-04-05 22:47:48 +0200

    Fix venues without reviews

--- src/components/modules/RestaurantDetail/components/Reviews.js
+++ src/components/modules/RestaurantDetail/components/Reviews.js
@@ -2,7 +2,12 @@ import React from 'react';
 import * as mc from './index';
 
 export const Reviews = ({ data }) => {
+  if (data == null) {
+    return null;
+  }
+
   const sortedReviews = data.sort((a, b) => a.time < b.time);
+
   return (
     <mc.ContentContainer>
       {sortedReviews.map(review => (
